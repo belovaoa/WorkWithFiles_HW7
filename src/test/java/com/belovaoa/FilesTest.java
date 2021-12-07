bf5db274078e580ce80dd7bd0889b39cf309810c
belova_oa <belovva.o@gmail.com>
2021-12-07 12:12:09 +0300

    change FilesTest

--- src/test/java/com/belovaoa/FilesTest.java
+++ src/test/java/com/belovaoa/FilesTest.java
@@ -12,11 +12,10 @@ import static com.codeborne.selenide.Selenide.open;
 @DisplayName("Тесты на файлы")
 public class FilesTest {
     @Test
-    @DisplayName("Имя файла должно отображаться после загрузки по абсолютному пути (не рекомендуется)")
+    @DisplayName("Имя файла должно отображаться после загрузки по относительному пути")
     void filenameShouldDisplayedUploadActionTest() {
         open("https://the-internet.herokuapp.com/upload");
-        File exampleFile = new File("/Users/belova_oa/IdeaProjects/WorkWithFiles_HW7src/test/resources/example.txt");
-        $("input[type='file']").uploadFile(exampleFile);
+        $("input[type='file']").uploadFromClasspath("example.txt");
         $("#file-submit").click();
         $("#uploaded-files").shouldHave(text("example.txt"));
     }
