ecec0eed4c2583392a8a0267b4deabf9cc45320a
belova_oa <belovva.o@gmail.com>
2021-12-19 14:53:25 +0300

    change FilesTest DownloadXls

--- src/test/java/com/belovaoa/HomeworkFilesTests.java
+++ src/test/java/com/belovaoa/HomeworkFilesTests.java
@@ -2,11 +2,13 @@ package com.belovaoa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.xlstest.XLS;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
@@ -17,6 +19,7 @@ import static helpers.FileContentReaderClass.FileContentReader1.readDocxFile;
 import static helpers.FileContentReaderClass.FileContentReader1.readFileContent;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DisplayName("Тесты на файлы")
@@ -43,12 +46,28 @@ public class HomeworkFilesTests {
     }
 
     @Test
-    @DisplayName("Скачивание пдф и проверка его содержимого")
+    @DisplayName("Скачивание .pdf и проверка его содержимого")
     void downloadPdfFileTest() throws IOException {
         open("http://school135.ru/priem-v-10-j-klass");
         File pdf = $("a[href*='/files/priem-10-klass/priem-10klass-2021.pdf']").download();
         PDF parsedPdf = new PDF(pdf);
         System.out.println();
         Assertions.assertEquals(3, parsedPdf.numberOfPages);
+    }
+
+    @Test
+    @DisplayName("Скачивание .xls и проверка содержимого файла")
+    void downloadXlsFileTest() throws FileNotFoundException {
+        open("https://ckmt.ru/price-download.html");
+        File file = $(byText("Скачать")).parent().download();
+        XLS parsedXls = new XLS(file);
+        System.out.println();
+        boolean checkPassed = parsedXls.excel
+                .getSheetAt(0)
+                .getRow(4)
+                .getCell(3)
+                .getStringCellValue()
+                .contains("www.сkmt.ru, msk@ckmt.ru");
+        assertTrue(checkPassed);
     }
 }
